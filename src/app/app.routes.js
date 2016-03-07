be04eea4d95d0997e7246380f195ae45b33da7fd
ethno2405 <k.tenekedzhiev@gmail.com>
2016-03-07 17:47:02 +0200

    Added projects component shell

--- src/app/app.routes.js
+++ src/app/app.routes.js
@@ -7,9 +7,18 @@
 
     pandoraApp.config(["$routeProvider",
     function ($routeProvider) {
-        $routeProvider.when("/login", {
-            templateUrl: "app/components/login/login.html",
-            controller: "LoginController"
-        });
+        $routeProvider
+            .when("/login", {
+                templateUrl: "app/components/login/login.html",
+                controller: "LoginController"
+            })
+            .when("/projects", {
+                templateUrl: "app/components/projects/projects.html",
+                controller: "ProjectsController",
+                requireToken: true
+            })
+            .otherwise({
+                redirectTo: '/login'
+            });
     }]);
 })();
